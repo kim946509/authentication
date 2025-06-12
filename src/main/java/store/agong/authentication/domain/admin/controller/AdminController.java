1ae9f11a2204dd0b3e919fc8299d2af53fea0a5f
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 16:06:40 +0900

    style : admin 컨트롤러 및 예시 dto 설정
    - swagger 스타일 설정

--- src/main/java/store/agong/authentication/domain/admin/controller/AdminController.java
+++ src/main/java/store/agong/authentication/domain/admin/controller/AdminController.java
@@ -1,5 +1,8 @@
 package store.agong.authentication.domain.admin.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +15,17 @@ import store.agong.authentication.domain.admin.response.GrantAdminResponse;
 import store.agong.authentication.domain.admin.service.UserGrantService;
 import store.agong.authentication.global.response.SuccessResponse;
 
+@Tag(name = "관리자", description = "관리자 전용 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
     private final UserGrantService userGrantService;
 
+    @Operation(
+            summary = "관리자 권한 부여",
+            description = "유저에게 관리자 권한을 부여합니다. 이 API는 SUPER_ADMIN 권한을 가진 사용자만 사용할 수 있습니다."
+    )
     @PatchMapping("/grant")
     public ResponseEntity<SuccessResponse<GrantAdminResponse>> grantAdmin(
             @RequestBody @Valid GrantAdminRequest request
@@ -26,3 +34,4 @@ public class AdminController {
         return ResponseEntity.ok(SuccessResponse.success(response));
     }
 }
+
