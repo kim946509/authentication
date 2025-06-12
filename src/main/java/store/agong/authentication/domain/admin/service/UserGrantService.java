c2bfbc7d9c71390be3b4e99c0411ef1176013f8b
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 17:33:18 +0900

    feat : 권한 변경시 강제로 재로그인 하도록 수정

--- src/main/java/store/agong/authentication/domain/admin/service/UserGrantService.java
+++ src/main/java/store/agong/authentication/domain/admin/service/UserGrantService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import store.agong.authentication.domain.admin.response.GrantAdminResponse;
+import store.agong.authentication.domain.forceReLogin.service.ForcedReLoginService;
 import store.agong.authentication.domain.user.entity.User;
 import store.agong.authentication.domain.user.enums.Role;
 import store.agong.authentication.domain.user.repository.UserRepository;
@@ -14,6 +15,7 @@ import store.agong.authentication.global.exception.BaseException;
 public class UserGrantService {
 
     private final UserRepository userRepository;
+    private final ForcedReLoginService forcedReLoginService;
 
     public GrantAdminResponse grantAdmin(String targetUsername) {
         User user = userRepository.findActiveByUsername(targetUsername)
@@ -21,6 +23,7 @@ public class UserGrantService {
 
         user.grantAdminRole();
         userRepository.update(user);
+        forcedReLoginService.force(user.getUsername());
 
         return new GrantAdminResponse(user.getUsername(), user.getRoles());
     }
