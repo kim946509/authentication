9585241ecc877b3e54678403ec1224966251bb21
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 17:10:14 +0900

    fix : 최신정보로 token을 재발급하도록 수정
    - claim 데이터를 항상 기존 토큰에서 가져왔음.
    - 이렇게 되면 권한이 변경되어도 반영이 안됨.
    - 토큰을 재발급할때는 항상 최신정보로 반영하도록 수정함

--- src/main/java/store/agong/authentication/domain/user/service/UserTokenReissueService.java
+++ src/main/java/store/agong/authentication/domain/user/service/UserTokenReissueService.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import store.agong.authentication.domain.user.dto.ReissueDto;
+import store.agong.authentication.domain.user.entity.User;
 import store.agong.authentication.domain.user.enums.Role;
+import store.agong.authentication.domain.user.repository.UserRepository;
 import store.agong.authentication.domain.user.response.ReissueResponse;
 import store.agong.authentication.global.exception.BaseException;
 import store.agong.authentication.global.jwt.provider.JwtTokenProvider;
@@ -17,32 +19,35 @@ public class UserTokenReissueService {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     public ReissueDto reissue(String refreshToken) {
 
-        // RefreshToken 유효성 검사
+        // 1. RefreshToken 유효성 검사
         if (!jwtTokenProvider.validateToken(refreshToken) || !jwtTokenProvider.isRefreshToken(refreshToken)) {
             throw new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다.");
         }
 
         String username = jwtTokenProvider.getUsername(refreshToken);
-        Set<Role> roles = jwtTokenProvider.getRoles(refreshToken);
 
+        // 2. 저장소에서 토큰 존재 여부 확인
         String storedRefreshToken = refreshTokenRepository.findByUsername(username)
                 .orElseThrow(() -> new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다."));
 
-        // 저장된 RefreshToken과 비교
         if (!storedRefreshToken.equals(refreshToken)) {
             throw new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다.");
         }
 
-        // 재발급
+        // 3. 최신 권한 정보 조회
+        User user = userRepository.findActiveByUsername(username)
+                .orElseThrow(() -> new BaseException(HttpStatus.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다."));
+        Set<Role> roles = user.getRoles();
+
+        // 4. 재발급
         String newAccessToken = jwtTokenProvider.generateAccessToken(username, roles);
         String newRefreshToken = jwtTokenProvider.generateRefreshToken(username, roles);
-
-        refreshTokenRepository.save(username, newRefreshToken); // 기존 토큰 대체
+        refreshTokenRepository.save(username, newRefreshToken);
 
         return new ReissueDto(newAccessToken, newRefreshToken);
     }
-
 }
