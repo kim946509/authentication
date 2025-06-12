0036ef2b3754fa6f68a52d3738a1fb27c67be45b
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 16:54:32 +0900

    fix : 관리자 권한부여 url 수정
    - restful하게 api 경로 수정

--- src/main/java/store/agong/authentication/domain/admin/controller/AdminController.java
+++ src/main/java/store/agong/authentication/domain/admin/controller/AdminController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,11 +51,11 @@ public class AdminController {
         }
         """
     )
-    @PatchMapping("/grant")
+    @PatchMapping("/users/{username}/roles")
     public ResponseEntity<SuccessResponse<GrantAdminResponse>> grantAdmin(
-            @RequestBody @Valid GrantAdminRequest request
+            @PathVariable("username") String username
     ) {
-        GrantAdminResponse response = userGrantService.grantAdmin(request.getUsername());
+        GrantAdminResponse response = userGrantService.grantAdmin(username);
         return ResponseEntity.ok(SuccessResponse.success(response));
     }
 }
