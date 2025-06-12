8fe0330911c8ae0e336ccdc57eeccb45eab8365e
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 18:21:21 +0900

    chore : 예외 메세지 수정
    - 토큰에 대한 상세한 정보는 보안에 취약하므로 공통된 에러 응답 메세지를 전달함.

--- src/main/java/store/agong/authentication/domain/user/service/UserTokenReissueService.java
+++ src/main/java/store/agong/authentication/domain/user/service/UserTokenReissueService.java
@@ -25,22 +25,22 @@ public class UserTokenReissueService {
 
         // 1. RefreshToken 유효성 검사
         if (!jwtTokenProvider.validateToken(refreshToken) || !jwtTokenProvider.isRefreshToken(refreshToken)) {
-            throw new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다.");
+            throw new BaseException(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않거나 만료되었습니다");
         }
 
         String username = jwtTokenProvider.getUsername(refreshToken);
 
         // 2. 저장소에서 토큰 존재 여부 확인
         String storedRefreshToken = refreshTokenRepository.findByUsername(username)
-                .orElseThrow(() -> new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다."));
+                .orElseThrow(() -> new BaseException(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않거나 만료되었습니다"));
 
         if (!storedRefreshToken.equals(refreshToken)) {
-            throw new BaseException(HttpStatus.UNAUTHORIZED, "RefreshToken이 유효하지 않습니다.");
+            throw new BaseException(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않거나 만료되었습니다");
         }
 
         // 3. 최신 권한 정보 조회
         User user = userRepository.findActiveByUsername(username)
-                .orElseThrow(() -> new BaseException(HttpStatus.NOT_FOUND, "유저 정보를 찾을 수 없습니다."));
+                .orElseThrow(() -> new BaseException(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않거나 만료되었습니다"));
         Set<Role> roles = user.getRoles();
 
         // 4. 재발급
